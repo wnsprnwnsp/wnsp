838aa31162e812a2d16b8261d71553cb403bbf9c
Jung Jin <filante96@naver.com>
2017-04-04 15:31:07 +0000

    20170405

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,5 @@ class HomeController < ApplicationController
     
      def write 
          @title = params[:title]
-         @content = params[:content]
-         @content1 = params[:content1]
      end
 end
